883316e7f4e5577f4d980084eaf45ca0d35cda20
Ivor <ikojun00@fesb.hr>
2025-03-29 22:42:20 +0100

    app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,43 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PrismaService } from './prisma.service';
+import { QuizController } from './quiz/quiz.controller';
+import { QuizModule } from './quiz/quiz.module';
+import { AuthModule } from './auth/auth.module';
+import { QuizService } from './quiz/quiz.service';
+import { CategoryController } from './category/category.controller';
+import { CategoryModule } from './category/category.module';
+import { QuestionController } from './question/question.controller';
+import { QuestionService } from './question/question.service';
+import { QuestionModule } from './question/question.module';
+import { ScoreService } from './score/score.service';
+import { ScoreController } from './score/score.controller';
+import { ScoreModule } from './score/score.module';
+import { CategoryService } from './category/category.service';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    QuizModule,
+    AuthModule,
+    CategoryModule,
+    QuestionModule,
+    ScoreModule,
+  ],
+  controllers: [
+    QuizController,
+    CategoryController,
+    QuestionController,
+    ScoreController,
+  ],
+  providers: [
+    PrismaService,
+    QuizService,
+    QuestionService,
+    ScoreService,
+    CategoryService,
+    UserService,
+  ],
 })
 export class AppModule {}
